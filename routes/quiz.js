9492dfe1f2987ab0b438466284539cb268ba154a
Jan Pieniążek <johnn93@wp.pl>
2019-10-28 18:21:19 +0100

    routing & menu

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('users', { title: 'Users' });
+    res.render('quiz', { title: 'Quiz' });
 });
 
 
