51458d0a274661782836171b060e87f9bd3d75f5
Jan Pieniążek <johnn93@wp.pl>
2019-11-21 16:04:08 +0100

    update

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+let usersNumber = '10';
+let usersGender = 'both';
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -7,6 +9,20 @@ router.get('/', (req, res) => {
 });
 
 
+/* POST home page. */
+router.post('/', (req, res) => {
+    const body = req.body;
+    console.log(body);
+    let usersNumber = body.usersNumber;
+    let usersGender = body.gender;
+    console.log(usersNumber, usersGender);
+    const url = `https://randomuser.me/api?results=${usersNumber}&gender=${usersGender === "both" ? "male,female" : usersGender}`;
+    console.log(url);
+
+    req.get(url);
+    res.send({ url });
+
+});
 
 
 module.exports = router;
